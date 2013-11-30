1bb1fcd9233b20a89e5ac2dd222a8d90281b3286
Johan <johan.alvaradohdz@gmail.com>
2013-11-30 13:39:42 -0600

    prepare environment for rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CharityEvents::Application.config.secret_key_base = 'd4b3ebad44126089e84d9ecb9de5ccb2fff3f91ca6863f4811bd2b44d4bd24e8d3c072d5df91bc84149ba2ce38b01c1147f18b87f773d6953bfd35610fc3b221'
