fd3dbddb74af42ebc0d08db121a9325f57d9dcbb
Johan <johan.alvaradohdz@gmail.com>
2013-11-30 21:25:06 -0600

    added method for order events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
   CATEGORIES = ["Information", "We want your Support", "Charity Dinner"]
+  paginates_per 50
+
+  def self.search(params)
+    order(updated_at: :desc).page params
+  end
+
 end
