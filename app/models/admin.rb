0d28c5d48e1b3e9d728e3544da501d2f95cdf5ab
Johan <johan.alvaradohdz@gmail.com>
2013-12-01 09:49:14 -0600

    added admin to events

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,5 @@
 class Admin < ActiveRecord::Base
+  has_many :events
   attr_accessor :username
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ CharityEvents::Application.routes.draw do
   root 'events#index'
   devise_for :admins, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   resources :events
+  namespace :admin do
+    resources :events
+  end
 end
